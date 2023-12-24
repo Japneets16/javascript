cd23b7fd0bb8df360ef284b4e312bad8a3425d6a
Japneets16 <138960681+Japneets16@users.noreply.github.com>
2023-12-24 16:12:46 +0530

    variables.js

---
+++ variables.js
@@ -0,0 +1,20 @@
+console.log(" hi this is japneet singh")
+console.log(' thanks chln vaste yrr ')
+
+var a=5;
+
+// var b=4;
+// var b=10;          //ehda mtlb hai ki var nu jdo asi update krde hnn taan oh error nhi show krda te naal hi updated value show krda hai 
+
+let b=43;
+//let b='l';       // asi let di values nu bar bar redeclare nhi kr sksde
+
+{
+    let b=20;
+    console.log(b);         //jehra let haiga oh bracket oriented haiga hai oh brac di value nu alg show krega te brac ton bahr di value nu  alg
+}
+console.log(b);
+
+// const k=10;        poore code ch asi const di value nu change nhi kr skde 
+
+
