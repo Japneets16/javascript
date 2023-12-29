42819411f53aefc9048dbc867fc3a770794fe970
Japneets16 <138960681+Japneets16@users.noreply.github.com>
2023-12-29 20:28:47 +0530

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,20 +1,20 @@
-console.log(" hi this is japneet singh")
-console.log(' thanks chln vaste yrr ')
+//variable is used to store the data 
+// uses(let, var, const )
 
-var a=5;
+// let age;
+// age =21;
 
-// var b=4;
-// var b=10;          //ehda mtlb hai ki var nu jdo asi update krde hnn taan oh error nhi show krda te naal hi updated value show krda hai 
-
-let b=43;
-//let b='l';       // asi let di values nu bar bar redeclare nhi kr sksde
-
-{
-    let b=20;
-    console.log(b);         //jehra let haiga oh bracket oriented haiga hai oh brac di value nu alg show krega te brac ton bahr di value nu  alg
-}
-console.log(b);
-
-// const k=10;        poore code ch asi const di value nu change nhi kr skde 
+//data types 
+let firstname="japneet"   //strings 
+let age =23;              // number 
+let student= true;        // boolean 
 
 
+console.log("hello" , firstname)
+console.log("you are " , age  , " years old ")
+console.log("enrolled:" , student)
+
+
+//way to access the id in the html from the js
+
+document.getElementById('p1').innerHTML='hello'+ firstname;
